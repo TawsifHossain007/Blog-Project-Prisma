d8e0918ba657044a5f4062e86ad540375693d7d9
Tawsif Hossain <tawsifhossain18.o@gmail.com>
2026-07-06 14:52:13 +0600

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+    port : process.env.PORT || 5000,
+    appUrl : process.env.APP_URL,
+    databaseUrl : process.env.DATABASE_URL,
+    bcryptSaltRounds : process.env.BCRYPT_SALT_ROUNDS,
+    jwtAccessSecret : process.env.JWT_ACCESS_SECRET,
+    jwtRefreshSecret : process.env.JWT_REFRESH_SECRET,
+    jwtAccessExpiration : process.env.JWT_ACCESS_EXPIRATION,
+    jwtRefreshExpiration : process.env.JWT_REFRESH_EXPIRATION,
+}
